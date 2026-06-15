0f832ad88f8df2f25ccef9de5cd8de6a3db03971
Lostcrown301 <lostcrown301@gmail.com>
2026-06-15 21:29:37 +0530

    /auth/me done

---
+++ app/auth/dependencies.py
@@ -0,0 +1,46 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.database.models.user import User
+from app.auth.jwt_handler import verify_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    try:
+        payload = verify_access_token(token)
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
+
+    user = db.query(User).filter(
+        User.id == int(user_id)
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return user
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.auth.dependencies import get_current_user
+from app.database.models.user import User
+
 from app.database.database import get_db
 from app.schemas.user import (
     UserCreate,
@@ -53,3 +56,12 @@ def login(
         email=user.email,
         password=user.password
     )
+
+@router.get(
+    "/me",
+    response_model=UserResponse
+)
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return current_user

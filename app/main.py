5fca62f1bef4fe97928448a2d9ec065a53772c08
Lostcrown301 <lostcrown301@gmail.com>
2026-06-16 11:08:05 +0530

    Added a basic frontend for testing

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from fastapi import FastAPI
 from app.routes.auth_routes import router as auth_router
 from app.database.database import Base, engine
 from app.database.models.user import User
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth_router)
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import api from "../services/api";
+
+
+
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+      localStorage.setItem(
+        "token",
+        response.data.access_token
+      );
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
